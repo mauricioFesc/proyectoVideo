d03ecd8146801acb3b41c47c852ee5bf27eeceb5
venomMau <mauricio.fesc@gmail.com>
2016-10-17 16:59:52 -0500

    agregando conteo negativos

--- conteo.c
+++ conteo.c
@@ -2,17 +2,25 @@
 #include<stdlib.h>
 int main()
 {
-  int conteo=0,num;
-  printf("\n\t Programa conteo de positivos");
+  int conteop=0,conteon=0,num;
+  printf("\n\t Programa conteo de positivos y negativos");
   printf("\n\t Ingrese un numero\t");
   scanf("%i",&num);
   while(num!=0)
   {
-     conteo++;
+     if(num>0)
+     {
+       conteop++;
+     }
+     else
+     {
+       conteon++;
+     }
+
      printf("\n\t Ingrese un numero\t");
      scanf("%i",&num);
   }
-  printf("\n\t Usted ingreso %i numeros enteros\n",conteo);
+  printf("\n\t Usted ingreso %i numeros enteros positivos y %i negativos\n",conteop,conteon);
   system("PAUSE");
   return 0;
 }
